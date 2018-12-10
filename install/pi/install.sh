d674f01f5eb746f3d573d47706dad6e4627d9ddc
nakajima.kouichi <nakajima.kouichi@subaru.co.jp>
2018-12-10 15:40:56 +0900

    autorope repo 反映

--- install/pi/install.sh
+++ install/pi/install.sh
@@ -1,21 +1,5 @@
 # Script to install everything needed for donkeycar except the donkeycar library
 
-
-# Get witch Pi version
-echo "Enter the Pi number (3 or 0)"
-read pi_num
-if  [ $pi_num == 3 ]; then
-  echo "installing for Pi 3."
-  tf_file=tensorflow-1.8.0-cp35-none-linux_armv7l.whl
-elif [ $pi_num == 0 ]; then
-  echo "installing for Pi Zero."
-  tf_file=tensorflow-1.8.0-cp35-none-linux_armv6l.whl
-else
-  echo "Only Pi 3 and Pi Zero are supported."
-  exit 1
-fi
-
-
 #standard updates (5 min)
 sudo apt update -y
 sudo apt upgrade -y
@@ -63,8 +47,4 @@ source ~/env/bin/activate
 pip install pandas #also installs numpy
 
 
-#install tensorflow (5 min)
-echo "Installing Tensorflow"
-wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.8.0/${tf_file}
-pip install ${tf_file}
-rm ${tf_file}
+pip install tensorflow==1.9
